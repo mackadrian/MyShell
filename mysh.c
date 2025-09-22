a01b6e8bb2df39aceb7a1d33b71cf9ece342816e
tarun jaswal <tjasw549@mtroyal.ca>
2025-09-22 22:49:42 +0000

    removed extra code

--- mysh.c
+++ mysh.c
@@ -1,29 +1,26 @@
 #include <unistd.h> // for read() and write() functions.
-#include <string.h>
+#include <string.h> //for strcomp
 
 int main() {
   char string_buffer[256];
   int bytes_read;
 
-  write(1,"$ ",2);
+  write(1, "$ ", 2);
   bytes_read = read(0, string_buffer, 256);
   string_buffer[bytes_read-1] = '\0';
-
+  
   while (strcmp(string_buffer, "exit") != 0) {
-    if (strcmp(string_buffer, "exit") == 0){
-      write(1, "quitting program\n", 17);
-    }
-      
-    else {
-      write (1, string_buffer, 256);
-      write (1, "\n", 1);
 
-      write(1, "$ ", 2);
-      bytes_read = read(0, string_buffer, 256);
-      string_buffer[bytes_read-1] = '\0';
-    }
+    write (1, string_buffer, 256);
+    write (1, "\n", 1);
+    
+    write(1, "$ ", 2);
+    bytes_read = read(0, string_buffer, 256);
+    string_buffer[bytes_read-1] = '\0';  
     
   }
+
+  write(1, "quitting program\n", 17);
   
   return 0;
-};
+}
