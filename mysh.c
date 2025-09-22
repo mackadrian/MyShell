3d79c95266912cac674f37835d036f9768b39b26
Mack Bautista <mbaut981@mtroyal.ca>
2025-09-22 20:50:36 +0000

    broken pls fixerino

--- mysh.c
+++ mysh.c
@@ -1,4 +1,5 @@
 #include <unistd.h> // for read() and write() functions.
+#include <string.h>
 
 int main() {
   char string_buffer[256];
@@ -6,13 +7,16 @@ int main() {
   write(1,"$ ", 2);
 
   while (strcmp(string_buffer, "exit") != 1) {
-    if (strcmp(string_buffer) == 0)
+    if (strcmp(string_buffer, "exit") == 0){
       // exit program
-  }
-    else {
+      }
+  else {
     write (1, string_buffer, 256);
     write (1, "\n", 1);
 
     
     }
-}
+  };
+
+  return 0;
+};
