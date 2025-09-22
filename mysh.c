0f520f6ee265583927b2ef3dffda7bd28428dba5
Jarod Dacocos <JarodDacocos@gmail.com>
2025-09-22 12:11:55 -0600

    added input loop

---
+++ mysh.c
@@ -0,0 +1,18 @@
+#include <unistd.h> // for read() and write() functions
+
+int main() {
+  char string_buffer[256];
+
+  write(1,"$ ", 2);
+
+  while (strcmp(string_buffer, "exit") != 1) {
+    if (strcmp(string_buffer) == 0)
+      // exit program
+  }
+    else {
+    write (1, string_buffer, 256);
+    write (1, "\n", 1);
+
+    
+    }
+}
