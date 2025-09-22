62f27d43e9882072758b0fba7cc44755a163f681
Mack Adrian Bautista <113152526+mackadrian@users.noreply.github.com>
2025-09-22 15:02:06 -0600

    Update mysh.c

--- mysh.c
+++ mysh.c
@@ -3,20 +3,27 @@
 
 int main() {
   char string_buffer[256];
+  int bytes_read;
 
+  bytes_read = read(0, string_buffer, 256);
+  string_buffer[bytes_read-1] = '\0';
   write(1,"$ ", 2);
 
   while (strcmp(string_buffer, "exit") != 1) {
     if (strcmp(string_buffer, "exit") == 0){
-      // exit program
+      write(1, "quitting program", 256)
       }
+      
   else {
     write (1, string_buffer, 256);
     write (1, "\n", 1);
 
+    write(1, "$ ", 2);
+    bytes_read = read(0, string_buffer, 256);
+    string_buffer[bytes_read-1] = '\0';
     
     }
-  };
-
+  }
+  
   return 0;
 };
