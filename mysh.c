4d64645fc7a751b73f14fb365d9e86422c90d805
tarun jaswal <tjasw549@mtroyal.ca>
2025-09-22 21:34:41 +0000

    some fixes

--- mysh.c
+++ mysh.c
@@ -5,24 +5,24 @@ int main() {
   char string_buffer[256];
   int bytes_read;
 
+  write(1,"$ ",2);
   bytes_read = read(0, string_buffer, 256);
   string_buffer[bytes_read-1] = '\0';
-  write(1,"$ ", 2);
 
-  while (strcmp(string_buffer, "exit") != 1) {
+  while (strcmp(string_buffer, "exit") != 0) {
     if (strcmp(string_buffer, "exit") == 0){
-      write(1, "quitting program", 256)
-      }
-      
-  else {
-    write (1, string_buffer, 256);
-    write (1, "\n", 1);
-
-    write(1, "$ ", 2);
-    bytes_read = read(0, string_buffer, 256);
-    string_buffer[bytes_read-1] = '\0';
-    
+      write(1, "quitting program\n", 17);
     }
+      
+    else {
+      write (1, string_buffer, 256);
+      write (1, "\n", 1);
+
+      write(1, "$ ", 2);
+      bytes_read = read(0, string_buffer, 256);
+      string_buffer[bytes_read-1] = '\0';
+    }
+    
   }
   
   return 0;
