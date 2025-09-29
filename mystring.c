b3875de7ed28fa50a19824263f17988bb94b49ca
Jarod Dacocos <JarodDacocos@gmail.com>
2025-09-29 16:44:31 -0600

    started function documentation

--- mystring.c
+++ mystring.c
@@ -1,6 +1,17 @@
 #include "mystring.h"
 
+/* ---
+Function Name: mystrlen
 
+Purpose: 
+  Returns the length of a string
+
+Input:
+  s - string
+
+Output:
+  len - 
+--- */
 unsigned int mystrlen(const char *s)
 {
   int length = 0;
@@ -11,7 +22,18 @@ unsigned int mystrlen(const char *s)
   return len;
 }
 
+/* ---
+Function Name: mystrcmp
 
+Purpose:
+  Compares two strings
+
+Input:
+  s1 - string
+
+Output:
+  str_compare - returns
+--- */
 int mystrcmp(const char *s1, const char *s2)
 {
   while (*s1 && (*s1 == *s2))
@@ -19,10 +41,25 @@ int mystrcmp(const char *s1, const char *s2)
       s1++;
       s2++;
     }
-  return (unsigned char)*s1 - (unsigned char)*s2;
+
+  int str_compare = (unsigned char)*s1 - (unsigned char)*s2;
+  
+  return str_compare
 }
 
+/* ---
+Function Name: mystrcpy
 
+Purpose: 
+  Copies the string from source into destination.
+
+Input:
+  dest - 
+  src - 
+
+Output:
+  dest - destination is returned
+--- */
 char *mystrcpy(char *dest, const char *src)
 {
   char *ptr = dest;
