6059ce72c3aae53322b490befcb7e76a8e005a1d
Jarod Dacocos <jdaco737@mtroyal.ca>
2025-09-29 15:40:20 +0000

    string library functions implemented

--- mystring.c
+++ mystring.c
@@ -1,20 +1,35 @@
 #include "mystring.h"
-#include <string.h>       /* TO DO: initial cheat! Remove this line and all library dependency evetually */
 
 
 unsigned int mystrlen(const char *s)
 {
-  return strlen(s);     /* TO DO: replace string library wrappers with non-library code */
+  int length = 0;
+  while (s[len] != '\0')
+    {
+    len++;
+    }
+  return len;
 }
 
 
 int mystrcmp(const char *s1, const char *s2)
 {
-  return strcmp(s1, s2);
+  while (*s1 && (*s1 == *s2))
+    {
+      s1++;
+      s2++;
+    }
+  return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
 
 char *mystrcpy(char *dest, const char *src)
 {
-  return strcpy(dest, src);
+  char *ptr = dest;
+  while (*src)
+    {
+      *ptr++ = *src++;
+    }
+  *ptr = '\0';
+  return dest;
 }
