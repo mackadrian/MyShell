82034decc1d5ebb1c5b8d8819ace2716ddc81d6a
Jarod Dacocos <JarodDacocos@gmail.com>
2025-10-15 18:27:17 -0600

    Update runjob.h

--- runjob.h
+++ runjob.h
@@ -5,7 +5,7 @@
 #include "mysh.h"
 
 void build_fullpath(char *buf, const char *dir, const char *cmd);
-void run_job (Job *job);
 char* resolve_command_path(const char *cmd, char *envp[]);
+void run_job (Job *job, char* envp[]);
 
 #endif
