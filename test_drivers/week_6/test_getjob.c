8ddfaf50bf9b2a8989404c3d192fcef4704a58e2
Mack Bautista <mbaut981@mtroyal.ca>
2025-10-16 03:05:50 +0000

    work on test_getjob

--- test_drivers/week_6/test_getjob.c
+++ test_drivers/week_6/test_getjob.c
@@ -4,28 +4,114 @@
 #include "errors.h"
 #include "jobs.h"
 
-#include <stdio.h>   /* only for testing output */
+#include <stdio.h>
 
-int main(void)
-{
-    Job job;
+//FUNCTION DECLARATIONS
+void print_job(Job *job);
+void parse_pipeline(Job *job, char *command_line);
+void test1();
+void test2();
+void test3();
+void test4();
 
-    printf("Enter command for get_job test: ");
-    get_job(&job);
 
-    printf("Number of stages: %d\n", job.num_stages);
-    printf("Background flag: %d\n", job.background);
+int main() {
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    return 0;
+}
 
-    for (int i = 0; i < job.num_stages; i++) {
-        Command *cmd = &job.pipeline[i];
-        printf("Stage %d argc: %d\n", i, cmd->argc);
+/* Helper to print a Job structure */
+void print_job(Job *job) {
+    printf("Number of stages: %d\n", job->num_stages);
+    printf("Background flag: %d\n", job->background);
+
+    for (int i = 0; i < job->num_stages; i++) {
+        Command *cmd = &job->pipeline[i];
+        printf("  Stage %d argc: %d\n", i, cmd->argc);
         for (int j = 0; j < cmd->argc; j++) {
-            printf("argv[%d]: %s\n", j, cmd->argv[j]);
+            printf("    argv[%d]: %s\n", j, cmd->argv[j]);
         }
     }
+    printf("-------------------------------------------------\n");
+}
+
+/* Helper to split a command line by pipes and call parse_stage for each stage */
+void parse_pipeline(Job *job, char *command_line) {
+    set_job(job);
+
+    /* Check for background execution */
+    int len = mystrlen(command_line);
+    if (len > 0 && command_line[len - 1] == '&') {
+        job->background = 1;
+        command_line[len - 1] = '\0';
+    }
+
+    int stage_start = 0;
+    for (int i = 0;; i++) {
+        char c = command_line[i];
+        if (c == '|' || c == '\0') {
+            command_line[i] = '\0';
+
+            /* Skip leading whitespace */
+            while (command_line[stage_start] == ' ' || command_line[stage_start] == '\t')
+                stage_start++;
+
+            if (command_line[stage_start] != '\0') {
+                parse_stage(&job->pipeline[job->num_stages],
+                            &command_line[stage_start],
+                            job);
+                job->num_stages++;
+            }
+
+            if (c == '\0')
+                break;
+
+            stage_start = i + 1;
+        }
+    }
+}
 
 
+void test1() {
+    Job job;
+    char command[] = "ls -l";
+    printf("Test 1: '%s'\n", command);
+    parse_pipeline(&job, command);
+    print_job(&job);
+}
 
-    
-    return 0;
+void test2() {
+    Job job;
+    char command[] = "cat file.txt | grep foo | sort";
+    printf("Test 2: '%s'\n", command);
+    parse_pipeline(&job, command);
+    print_job(&job);
+}
+
+void test3() {
+    Job job;
+    char command[] = "echo hello world &";
+    printf("Test 3: '%s'\n", command);
+    parse_pipeline(&job, command);
+    print_job(&job);
+}
+
+void test4() {
+    Job job;
+    char command[] = "gcc main.c -o main > output.txt";
+    printf("Test 4: '%s'\n", command);
+    parse_pipeline(&job, command);
+    print_job(&job);
+}
+
+void test5() {
+    Job job;
+    char command[] = "sort < unsorted.txt | uniq | wc -l";
+    printf("Test 5: '%s'\n", command);
+    parse_pipeline(&job, command);
+    print_job(&job);
 }
