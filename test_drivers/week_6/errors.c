8b24d5ae3c4a437812727dda64dff0a1ecdb71ce
Mack Bautista <mbaut981@mtroyal.ca>
2025-10-16 04:37:27 +0000

    made header for print_error:

--- test_drivers/week_6/errors.c
+++ test_drivers/week_6/errors.c
@@ -2,6 +2,16 @@
 #include "mystring.h"
 #include <unistd.h>
 
+/* ---
+Function Name: print_error
+Purpose:
+    Prints a descriptive error message corresponding to the provided ErrorCode.
+Input:
+    code - an enum value representing the specific error to print
+Output:
+    Writes the error message to standard error (STD_ERR). If the code
+    is invalid or has no associated message, nothing is printed.
+--- */
 void print_error(enum ErrorCode code) {
     if (code > 0 && code < NUM_ERRORS && error_messages[code]) {
         write(STD_ERR, error_messages[code], mystrlen(error_messages[code]));
