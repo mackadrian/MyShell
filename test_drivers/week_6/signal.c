996bde1840529c992773f6400d9d5eec776a22c3
Jarod Dacocos <JarodDacocos@gmail.com>
2025-10-17 16:40:03 -0600

    Update signal.c

--- test_drivers/week_6/signal.c
+++ test_drivers/week_6/signal.c
@@ -15,3 +15,28 @@ void handle_signal(int sig)
     }
   }
 }
+
+/* ---
+Function Name: initialize_signal_handler
+
+Purpose:
+  Installs the shell's signal handlers. Handles Ctrl+C (SIGINT)
+  with handle_signal, and ignores Ctrl+Z (SIGTSTP) to prevent
+  background suspension of the shell.
+
+Input:
+  None
+
+Output:
+  None
+--- */
+void initialize_signal_handler()
+{
+    struct sigaction sa;
+    sa.sa_handler = handle_signal;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);  // handle ^C
+
+    signal(SIGTSTP, SIG_IGN);      // ignore ^Z
+}  
