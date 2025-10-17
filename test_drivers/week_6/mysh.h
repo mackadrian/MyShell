14cdc5f2386c7e9e86934d436dd2ca9029f31794
Jarod Dacocos <JarodDacocos@gmail.com>
2025-10-17 17:58:51 -0600

    Update mysh.h

--- test_drivers/week_6/mysh.h
+++ test_drivers/week_6/mysh.h
@@ -11,6 +11,11 @@
 #define STD_OUT 1
 #define STD_ERR 2
 
+#define FALSE_VALUE             0
+#define TRUE_VALUE              1
+#define WAIT_ANY_CHILD          (-1)
+#define NULL_PTR                ((char **)0)
+
 extern int fg_job_status;
 
 static void remove_zombies();
