f5f6f1b8106ff96455c8548dfa9e6b1afb5c2b21
Mack Bautista <mbaut981@mtroyal.ca>
2025-10-18 23:47:36 +0000

    fixed hard coded literals in mystring.c

--- mystring.h
+++ mystring.h
@@ -1,8 +1,17 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
 
-#define NULL_CHAR       '\0'
-#define ZERO_VALUE      0
+// General Constants
+#define ZERO_VALUE          0       
+#define INITIAL_INDEX       0       
+#define NULL_CHAR           '\0'    
+#define DECIMAL_BASE        10      
+#define NEGATIVE_SIGN       '-'     
+#define ZERO_CHAR           '0'     
+#define HALF                2       
+#define TRUE                1       
+#define FALSE               0       
+
 
 unsigned int mystrlen(const char *s);
 int mystrcmp(const char *s1, const char *s2);
